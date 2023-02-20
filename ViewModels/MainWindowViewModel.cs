c9b0e0e6f80ea5a71f2979d9f8d2bea967a8ffec
Oliver Huckfeldt <oliverhuckfeldt@gmail.com>
2023-02-21 00:48:56 +0100

    Projektdateien hinzufügen.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,8 @@
+﻿
+namespace FileExplorer.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace FileExplorer.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
